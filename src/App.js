505052e4818964016f67ca841e07865cd603b410
Matt Javier <matthewrjavier@gmail.com>
2020-09-18 00:14:32 -0700

    more button, links to github and linkedin, nav

--- src/App.js
+++ src/App.js
@@ -4,15 +4,108 @@ import About from './pages/About'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
 
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import MoreIcon from '@material-ui/icons/MoreVert'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Fade from '@material-ui/core/Fade'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    padding: theme.spacing(2, 2, 0),
+  },
+  paper: {
+    paddingBottom: 50,
+  },
+  list: {
+    marginBottom: theme.spacing(2),
+  },
+  subheader: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  fabButton: {
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  },
+}))
+
 const App = () => {
+
+  const classes = useStyles()
+
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  
   return (
     <Router>
       <div>
-        <nav>
+        {/* <nav>
           <Link to="/">About</Link>
           <Link to="/portfolio">Portfolio</Link>
           <Link to="/contact">Contact</Link>
-        </nav>
+        </nav> */}
+        <CssBaseline />
+        <AppBar>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" className={classes.appBar}>
+              <MenuIcon />
+            </IconButton>
+            <div className={classes.grow} />
+            <Typography className={classes.text} variant="h4" gutterBottom>
+              Matthew Javier
+            </Typography>
+            <div className={classes.grow} />
+            <IconButton aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick} edge="end" color="inherit">
+              <MoreIcon />
+            </IconButton> 
+            <Menu
+              id="fade-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+            >
+              <MenuItem onClick={handleClose} >
+                <a href="https://linkedin.com/in/mattjavier7" target="_blank">
+                  <LinkedInIcon />
+                </a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="https://github.com/mattjavier" target="_blank">
+                  GitHub <GitHubIcon />
+                </a>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route exact path="/" component={About} />
           <Route path="/portfolio" component={Portfolio} />
