a254a5b7c3567ee325b763519a3fa8b95e1134ad
Matt Javier <matthewrjavier@gmail.com>
2020-09-20 03:26:00 -0700

    footer component complete

--- src/App.js
+++ src/App.js
@@ -3,58 +3,11 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import About from './pages/About'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
-
-import { makeStyles } from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import MoreIcon from '@material-ui/icons/MoreVert'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
-import Fade from '@material-ui/core/Fade'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import GitHubIcon from '@material-ui/icons/GitHub';
 
-const useStyles = makeStyles((theme) => ({
-  text: {
-    padding: theme.spacing(2, 2, 0),
-  },
-  paper: {
-    paddingBottom: 50,
-  },
-  list: {
-    marginBottom: theme.spacing(2),
-  },
-  subheader: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  appBar: {
-    top: 'auto',
-    bottom: 0,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-}))
+import Footer from './components/Footer'
 
 const App = () => {
-
-  const classes = useStyles()
-
-  const [anchorEl, setAnchorEl] = React.useState(null)
-  const open = Boolean(anchorEl)
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-  
   return (
     <Router>
       <div>
@@ -64,45 +17,13 @@ const App = () => {
           <Link to="/contact">Contact</Link>
         </nav> */}
         <CssBaseline />
-        <AppBar>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" className={classes.appBar}>
-              <MenuIcon />
-            </IconButton>
-            <div className={classes.grow} />
-            <Typography className={classes.text} variant="h4" gutterBottom>
-              Matthew Javier
-            </Typography>
-            <div className={classes.grow} />
-            <IconButton aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick} edge="end" color="inherit">
-              <MoreIcon />
-            </IconButton> 
-            <Menu
-              id="fade-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={open}
-              onClose={handleClose}
-              TransitionComponent={Fade}
-            >
-              <MenuItem onClick={handleClose} >
-                <a href="https://linkedin.com/in/mattjavier7" target="_blank">
-                  <LinkedInIcon />
-                </a>
-              </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <a href="https://github.com/mattjavier" target="_blank">
-                  GitHub <GitHubIcon />
-                </a>
-              </MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
+        
         <Switch>
           <Route exact path="/" component={About} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   )
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MoreIcon from '@material-ui/icons/MoreVert'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Fade from '@material-ui/core/Fade'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    padding: theme.spacing(2, 2, 0),
+  },
+  paper: {
+    paddingBottom: 50,
+  },
+  footer: {
+    background: 'linear-gradient(45deg, #1cb5e0 30%, #000046 90%)',
+    top: 'auto',
+    bottom: 0,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  link: {
+    color: '#000046',
+    textDecoration: 'none',
+  },
+}))
+
+const Footer = () => {
+
+  const classes = useStyles()
+
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <AppBar className={classes.footer}>
+      <Toolbar>
+        <Typography variant="overline">
+          &copy; Matthew Javier
+        </Typography>
+        <div className={classes.grow} />
+        <IconButton aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick} edge="end" color="inherit">
+          <MoreIcon />
+        </IconButton> 
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose} >
+            <a
+              className={classes.link} 
+              href="https://linkedin.com/in/mattjavier7" target="_blank">
+              <LinkedInIcon />
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a
+              className={classes.link} 
+              href="https://github.com/mattjavier" 
+              target="_blank">
+              <GitHubIcon />
+            </a>
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Footer
