d21617829a0bdd9307a1172240fef616ea2d953f
Matt Javier <matthewrjavier@gmail.com>
2020-09-20 00:25:58 -0700

    material-ui cdn

--- src/App.js
+++ src/App.js
@@ -38,14 +38,6 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
-  fabButton: {
-    position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
-  },
 }))
 
 const App = () => {
