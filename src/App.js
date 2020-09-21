e7397426dc3818b2f2f87c011db8d0ebaeffd00a
Matt Javier <matthewrjavier@gmail.com>
2020-09-20 18:54:58 -0700

    set background to dark

--- src/App.js
+++ src/App.js
@@ -4,28 +4,37 @@ import About from './pages/About'
 import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles' 
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
+const dark = createMuiTheme({
+  palette: {
+    background: {
+      default: '#202020'
+    },
+    text: {
+      primary: '#ffffff'
+    }
+  }
+})
+
 const App = () => {
   return (
     <Router>
-      <div>
-        {/* <nav>
-          <Link to="/">About</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contact">Contact</Link>
-        </nav> */}
-        <CssBaseline />
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={About} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-        <Footer />
-      </div>
+      <MuiThemeProvider theme={dark}>
+        <div>
+          <CssBaseline />
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+          <Footer />
+        </div>
+      </MuiThemeProvider>
     </Router>
   )
 }
