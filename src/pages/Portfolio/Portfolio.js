fb197141a66a2267f49be10597a3301a9cf4fbe7
Matt Javier <matthewrjavier@gmail.com>
2020-09-22 00:06:56 -0700

    portfolio grid start, uploaded pictures

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,8 +1,68 @@
 import React from 'react'
 
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import { Button, makeStyles } from '@material-ui/core'
+
+import CardMedia from '@material-ui/core/CardMedia'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: 50
+  },
+  title: {
+    letterSpacing: 4
+  }
+}))
+
 const Portfolio = () => {
+  const classes = useStyles()
+
   return (
-    <h1>This is the Portfolio</h1>
+    <Grid 
+      container
+      direction="column"
+      justify="space-around"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Typography
+        variant="h2"
+        className={classes.title}
+      >
+        Projects
+      </Typography>
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+        >
+        <Grid item xs={12} sm={6}>
+          <Button>
+            <CardMedia
+              image="../../images/coview/coview-home.png" 
+            />
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+        >
+        <Grid item xs={12} sm={6}>
+          
+        </Grid>
+        <Grid item xs={12} sm={6}>
+
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
