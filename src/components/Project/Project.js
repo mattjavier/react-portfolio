1ac3bd4e2d8464b604b6a23400faf0d8e9d0836e
Matt Javier <matthewrjavier@gmail.com>
2020-09-22 10:02:31 -0700

    begin project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,26 +1,52 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core'
+
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardActions from '@material-ui/core/CardActions'
 
+const useStyles = makeStyles((theme) => ({
+  image: {
+    height: 300,
+    width: 300,
+  },
+  card: {
+    textAlign: 'center',
+  },
+  buttons: {
+    color: '#3097a5',
+    backgroundColor: '#1a1a1a',
+    '&:hover': {
+      color: '#f55457'
+    },
+  },
+  cardAction: {
+    backgroundColor: '#1a1a1a'
+  }
+}))
+
 const Project = props => {
-  <Card className={classes.card}>
-            <CardMedia
-              className={classes.image}
-              image={coviewImg}
-              title="Coview-19"
-            />
-            <CardActions className={classes.cardAction}>
-              <Button 
-                size="small" 
-                className={classes.buttons}
-              >
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.image}
+        image={props.imageURL}
+        title={props.name}
+      />
+      <CardActions className={classes.cardAction}>
+        <Button 
+          size="small" 
+          className={classes.buttons}
+        >
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  )
 }
 
 export default Project
