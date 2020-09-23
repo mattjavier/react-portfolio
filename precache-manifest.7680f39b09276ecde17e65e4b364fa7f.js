1e78246b59c9efddf8cc978bee85a720ccb507c0
Matt Javier <matthewrjavier@gmail.com>
2020-09-22 22:59:51 -0700

    Updates

---
+++ precache-manifest.7680f39b09276ecde17e65e4b364fa7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ca3c8399ded0b6984c3321944e9d70",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "51a8c5cee392b6a82b86",
+    "url": "/react-portfolio/static/js/2.f129de0f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.f129de0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0927c79129b104bb717f",
+    "url": "/react-portfolio/static/js/main.d38a1159.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "5fd6d1e7dbfb770ba1ca39761f225314",
+    "url": "/react-portfolio/static/media/IMG_1803.5fd6d1e7.JPG"
+  },
+  {
+    "revision": "f64ab94c95a90434020ea58b2664cceb",
+    "url": "/react-portfolio/static/media/coview-home.f64ab94c.png"
+  },
+  {
+    "revision": "f3ff12c192b5f1799b61b2b12811d18c",
+    "url": "/react-portfolio/static/media/quiz-start.f3ff12c1.png"
+  },
+  {
+    "revision": "7096e251b457308877b167b5d6f44c45",
+    "url": "/react-portfolio/static/media/sensify1.7096e251.png"
+  },
+  {
+    "revision": "5f02cddef3e9b66aea48ee7e087d64b2",
+    "url": "/react-portfolio/static/media/tvt.5f02cdde.png"
+  }
+]);
